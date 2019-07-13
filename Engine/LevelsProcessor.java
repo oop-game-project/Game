c0ddb52d79d67d8a66e67661a8f7fadc902e4057
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-07-13 16:19:48 +0500

    Another SinglePlayerLevel structure update

--- Engine/LevelsProcessor.java
+++ Engine/LevelsProcessor.java
@@ -2,25 +2,32 @@ package Game.Engine;
 
 import Game.Engine.GameObjects.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelsProcessor
 {
     public class SinglePlayerLevel
     {
         public final int[] gameFieldSize;
 
-        public GameObjects.Player player;
-        public GameObjects.MovableObject[] mobs;
-        public GameObjects.MovableObject[] projectiles;
-        public GameObjects.InterfaceObject[] interfaceObjects;
+        public Player player;
+        public List<MovableObject> mobs;
+        public List<MovableObject> projectiles;
+        public List<InterfaceObject> interfaceObjects;
 
         public SinglePlayerLevel(
             int[] inputGameFieldSize,
             Player inputPlayer,
-            MovableObject[] inputMobs)
+            List<MovableObject> inputMobs,
+            List<MovableObject> inputProjectiles,
+            List<InterfaceObject> inputInterfaceObjects)
         {
             this.gameFieldSize = inputGameFieldSize;
             this.player = inputPlayer;
             this.mobs = inputMobs;
+            this.projectiles = inputProjectiles;
+            this.interfaceObjects = inputInterfaceObjects;
         }
     }
 
@@ -34,19 +41,26 @@ public class LevelsProcessor
 
         int[] gameFieldSize = new int[] { 700, 700, 0 };
 
-        Player player = gameObjects.new Player(new int[] { 175, 350, 0 }, 20);
+        Player player = gameObjects.new Player(
+            new int[] { 175, 350, 0 },
+            20);
 
-        MovableObject[] mobs = new GameObjects.MovableObject[3];
-        mobs[0] = gameObjects.new SphereMob(
+        List<MovableObject> mobs = new ArrayList<>();
+        mobs.add(gameObjects.new SphereMob(
             new int[]{420, 175, 0},
-            5);
-        mobs[1] = gameObjects.new SphereMob(
+            5));
+        mobs.add(gameObjects.new SphereMob(
             new int[]{350, 250, 0},
-            5);
-        mobs[2] = gameObjects.new SphereMob(
+            5));
+        mobs.add(gameObjects.new SphereMob(
             new int[]{420, 420, 0},
-            5);
+            5));
 
-        return new SinglePlayerLevel(gameFieldSize, player, mobs);
+        return new SinglePlayerLevel(
+            gameFieldSize,
+            player,
+            mobs,
+            new ArrayList<>(),
+            new ArrayList<>());
     }
 }
