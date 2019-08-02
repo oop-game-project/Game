46f13192e306946786d6bc91f5ee75dcfab5a2ff
Fourand <4randrofficial@gmail.com>
2019-08-02 21:03:40 +0500

    add collisions

--- Engine/CollisionsProcessor.java
+++ Engine/CollisionsProcessor.java
@@ -8,50 +8,72 @@ public class CollisionsProcessor {
     private int sizeX;
     private int sizeY;
     private int sizeZ;
+    private int playerX;
+    private int playerY;
 
     public CollisionsProcessor(int[] gameFieldSize) {
 
         sizeX = gameFieldSize[0];
         sizeY = gameFieldSize[1];
         sizeZ = gameFieldSize[2];
+        playerX = 175;
+        playerY = 350;
     }
 
     public Collision getCollision(SinglePlayerLevel currentLevel, int[] moveVector, MovableObject movableObject) {
 
         int x = movableObject.currentLocation[0];
         int y = movableObject.currentLocation[1];
-//        int z = movableObject.currentLocation[2]; ?
+        int z = movableObject.currentLocation[2];
         int vectorX = moveVector[0];
         int vectorY = moveVector[1];
-//        int vectorZ = moveVector[2]; ?
-        if (movableObject instanceof Player) {
+        int vectorZ = moveVector[2];
 
-            if (playerOutOfHorizontalBorders(vectorX, x) && playerOutOfVerticalBorders(vectorY, y))
-                return new Collision(movableObject, GameEvent.OUT_DIAGONAL, null);
-            if (playerOutOfHorizontalBorders(vectorX, x))
-                    return new Collision(movableObject, GameEvent.OUT_HORIZONTAL, null);
-            if (playerOutOfVerticalBorders(vectorY, y))
-                return new Collision(movableObject, GameEvent.OUT_VERTICAL, null);
+        if (movableObject instanceof Player) {
+            playerX = x;
+            playerY = y;
+            return getPlayerCollision(x, y, vectorX, vectorY, (Player)movableObject);
         }
         if (movableObject instanceof BasicProjectile) {
-
-            if ((y + vectorY + PaintingConst.BASIC_PROJECTILE_DIAMETER > sizeY) ||
-                    (y + vectorY - PaintingConst.BASIC_PROJECTILE_DIAMETER) < 0)
-                return new Collision(movableObject, GameEvent.BASIC_PROJECTILE_IS_OUT, null);
+            return getBasicProjectileCollision(x, y, vectorX, vectorY, (BasicProjectile)movableObject);
         }
+        throw new IllegalArgumentException("unknown movableObject");
+    }
+
+    private Collision getPlayerCollision(int x, int y, int vectorX, int vectorY, Player movableObject) {
+        if (playerOutOfHorizontalBorders(vectorX, x) && playerOutOfVerticalBorders(vectorY, y))
+            return new Collision(movableObject, GameEvent.OUT_DIAGONAL, null);
+        if (playerOutOfHorizontalBorders(vectorX, x))
+            return new Collision(movableObject, GameEvent.OUT_HORIZONTAL, null);
+        if (playerOutOfVerticalBorders(vectorY, y))
+            return new Collision(movableObject, GameEvent.OUT_VERTICAL, null);
         return new Collision(movableObject, GameEvent.OK, null);
     }
 
     private boolean playerOutOfHorizontalBorders(int vector, int player) {
-
         return (player + vector + PaintingConst.PLAYER_SIDE_LENGTH / 2 > sizeX)
                 || (player + vector - PaintingConst.PLAYER_SIDE_LENGTH / 2 < 0);
     }
 
     private boolean playerOutOfVerticalBorders(int vector, int player) {
-
         return (player + vector + PaintingConst.PLAYER_SIDE_LENGTH > sizeY)
                 || (player + vector < 0);
+    }
+
+    private Collision getBasicProjectileCollision(int x, int y, int vectorX, int vectorY, BasicProjectile movableObject) {
+        int projectileX = x + vectorX + PaintingConst.BASIC_PROJECTILE_DIAMETER;
+        int projectileY = y + vectorY + PaintingConst.BASIC_PROJECTILE_DIAMETER;
+
+        if (projectileY > sizeY || projectileY < 0)
+            return new Collision(movableObject, GameEvent.BASIC_PROJECTILE_IS_OUT, null);
+        if (!movableObject.firedByPlayer &&
+                (projectileX <= playerX + PaintingConst.PLAYER_SIDE_LENGTH &&
+                        projectileX >= playerX - PaintingConst.PLAYER_SIDE_LENGTH) &&
+                (projectileY <= playerY + PaintingConst.PLAYER_SIDE_LENGTH &&
+                        projectileY >= playerY - PaintingConst.PLAYER_SIDE_LENGTH))
+            return new Collision(movableObject, GameEvent.BASIC_PROJECTILE_TOUCHED_PLAYER, null);
+
+        return new Collision(movableObject, GameEvent.OK, null);
     }
 
     public enum GameEvent {
@@ -59,7 +81,8 @@ public class CollisionsProcessor {
         OUT_VERTICAL,
         OUT_HORIZONTAL,
         OUT_DIAGONAL,
-        BASIC_PROJECTILE_IS_OUT
+        BASIC_PROJECTILE_IS_OUT,
+        BASIC_PROJECTILE_TOUCHED_PLAYER
     }
 
     public class Collision {
